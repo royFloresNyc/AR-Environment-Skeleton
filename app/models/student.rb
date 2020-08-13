54bf7d1d926ec0f0b7e612485278a076bf3b61cc
royFloresNyc <royfloresnyc@gmail.com>
2020-08-13 15:50:32 -0400

    Student.all_in_grade method

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,9 @@ class Student < ActiveRecord::Base
     def full_name
         "#{self.first_name} #{self.last_name}"
     end 
+
+    def self.all_in_grade(grade)#expecting a string as argument
+        all.filter{ |student| student.grade_level == grade}
+    end 
+
 end 
--- db/migrate/20200813180056_add_teacher_to_students.rb
+++ db/migrate/20200813180056_add_teacher_to_students.rb
@@ -2,4 +2,4 @@ class AddTeacherToStudents < ActiveRecord::Migration[6.0]
   def change
      add_column :students, :teacher_id, :integer
   end
-end
+endra
