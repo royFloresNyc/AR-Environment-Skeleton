abff62003f0522db6939ab7a6ebe55362442feaf
kain <kaindoneson@gmail.com>
2020-08-14 15:11:43 -0400

    added in students_in_grade_level for clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,4 +2,8 @@ class Club < ActiveRecord::Base
     belongs_to :teacher
     has_many :student_clubs;
     has_many :students, through: :student_clubs;
+
+    def students_in_grade_level(grade_level_string)
+        self.students.filter{|s| s.grade_level == grade_level_string};
+    end
 end
